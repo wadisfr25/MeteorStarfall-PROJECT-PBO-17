5ddeea3edfb72f0b9609d4217c96fa35c4649826
B4R0N9 <danuartawiraguna@gmail.com>
2025-12-03 00:29:25 +0800

    Update HealthPickup.java

--- src/HealthPickup.java
+++ src/HealthPickup.java
@@ -1,8 +1,20 @@
 import java.awt.*;
 
-public class HealthPickup extends FallingObject {
+public class HealthPickup extends FallingObject implements Collidable {
 
     public HealthPickup(int x, int y, double speed, Image img) {
         super(x, y, speed, img);
     }
-}
+
+    @Override
+    public void update() {
+        y += speed;
+    }
+
+    @Override
+    public boolean collidesWith(Player p, GamePanel panel) {
+        return panel.pixelPerfectCollision(
+                p.getImage(), p.getX(), p.getY(),
+                this.image, this.x, this.y
+        );
+    }
